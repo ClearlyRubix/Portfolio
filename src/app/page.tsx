acd567464b38ad77693699f9f48b450a02b234f3
Cayden <clearlyrubix@gmail.com>
2024-04-25 02:56:37 -0600

    Hero and About Sections added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-zinc-900">
-      <Navbar />
-      Home Page
-    </main>
-  );
+    return (
+        <main className="min-h-screen bg-zinc-900">
+            <Navbar />
+            <div className="flex flex-col md:flex-row">
+                <div className="basis-1/2 self-stretch">
+                    <HeroSection />
+                </div>
+                <div id="about" className="basis-1/2 self-stretch">
+                    <AboutSection />
+                </div>
+            </div>
+        </main>
+    );
 }
