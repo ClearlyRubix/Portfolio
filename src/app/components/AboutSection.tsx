5123372b993f0895153db53997f7c4fd43a353de
Cayden <clearlyrubix@gmail.com>
2024-04-25 12:35:53 -0600

    about tag moved to the aboutsection component

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -3,7 +3,7 @@ import AboutText from "./AboutText"
 const AboutSection = () => {
 
     return (
-        <div className="border-2 border-zinc-800 rounded m-4 p-4 h-full">
+        <div id="about" className="border-2 border-zinc-800 rounded m-4 p-4 h-full">
             <h2 className="font-bold text-sm text-zinc-100 uppercase tracking-widest pb-6">
                 About Me
             </h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import HeroSection from "./components/HeroSection";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  
     return (
         <main className="min-h-screen bg-zinc-900">
             <Navbar />
@@ -11,8 +16,8 @@ export default function Home() {
                 <div className="basis-1/2 self-stretch">
                     <HeroSection />
                 </div>
-                <div id="about" className="basis-1/2 self-stretch">
-                    <AboutSection />
+                <div className="basis-1/2 self-stretch">
+                    <AboutSection/>
                 </div>
             </div>
         </main>
