c89f05f93ca0206386f00b39d09cfcac19aa4810
Cayden <clearlyrubix@gmail.com>
2024-04-25 02:56:50 -0600

    About Section Created

---
+++ src/app/components/AboutSection.tsx
@@ -0,0 +1,15 @@
+import AboutText from "./AboutText"
+
+const AboutSection = () => {
+
+    return (
+        <div className="border-2 border-zinc-800 rounded m-4 p-4 h-full">
+            <h2 className="font-bold text-sm text-zinc-100 uppercase tracking-widest pb-6">
+                About Me
+            </h2>
+            <AboutText/>  
+        </div>
+    )
+}
+
+export default AboutSection
---
+++ src/app/components/AboutText.tsx
@@ -0,0 +1,17 @@
+const AboutText = () => {
+
+    return (
+        <>
+        <div>
+            <p className="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu elit a purus mollis mollis. Suspendisse cursus condimentum nulla, non dictum quam ultricies quis. Quisque mauris enim, viverra vel nunc ac, imperdiet vehicula lectus. Duis vestibulum, ipsum in faucibus tincidunt, urna justo rutrum magna, sed cursus lorem mauris imperdiet justo. Pellentesque ut eros a mi aliquam vulputate. Sed volutpat risus in sodales vehicula. Vivamus tristique mauris eget augue dapibus, in laoreet elit ullamcorper. Aenean aliquam felis eget pulvinar varius. Donec fermentum, justo eget aliquam porttitor, dui lorem molestie lectus, eu varius purus est et tortor. Pellentesque orci leo, posuere sed rutrum quis, malesuada sit amet mi.
+            </p>
+            <p>
+                Duis nisi lacus, egestas vel ipsum et, maximus egestas dui. Curabitur felis ipsum, lobortis nec nisi nec, efficitur cursus enim. Integer a urna hendrerit, pulvinar orci sed, laoreet velit. Integer volutpat viverra arcu, eget sagittis mi faucibus eget. Fusce at enim vel nisl tincidunt euismod lobortis eget nibh. Cras sollicitudin, turpis at convallis imperdiet, nibh lectus vestibulum felis, quis malesuada magna felis at metus. Suspendisse auctor ornare sagittis. Donec eu erat vel quam aliquet vestibulum nec quis ipsum. Nunc non leo a nunc venenatis cursus.
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default AboutText
