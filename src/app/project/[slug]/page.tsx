ff685147cf9fc709aa99a1c4c404c34e651c1c54
Cayden <clearlyrubix@gmail.com>
2024-04-25 21:29:24 -0600

    begin project structure

---
+++ src/app/project/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+export default function Project() {
+    
+    const router = useRouter()
+
+
+    return (
+        <main className="">
+            <div className="flex flex-col md:flex-row">
+                {}
+            </div>
+        </main>
+    );
+}
