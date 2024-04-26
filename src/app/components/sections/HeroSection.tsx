cf16b174fcdaa8715da185c4e57fb69992141673
Cayden <clearlyrubix@gmail.com>
2024-04-25 20:35:36 -0600

    fix styling

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -8,8 +8,7 @@ import IconLinkedIn from "../icons/IconLinkedIn"
 
 const HeroSection = () => {
     return (
-        <>
-        <div className="flex flex-col border-2 border-zinc-800 rounded m-4 p-4 h-full">
+        <div className="flex flex-col border-2 border-zinc-800 rounded m-4 p-4">
             <h1 className="text-zinc-200 font-bold text-4xl tracking-tighter mb-4">
                 Cayden Batten
             </h1>
@@ -26,7 +25,7 @@ const HeroSection = () => {
                     </Link>
                 </li>
             </ul>
-            <ul className="flex items-center mt-12 md:mt-auto">
+            <ul className="flex items-center mt-8">
                 <li className="mr-5 shrink-0 transition-transform hover:-translate-y-1">
                     <Link className="hover:text-zinc-200" href={"https://www.linkedin.com/in/caydenbatten/"} target="_blank" title="LinkedIn">
                         <IconLinkedIn feather={true}/>
@@ -39,7 +38,6 @@ const HeroSection = () => {
                 </li>
             </ul>
         </div>
-        </>
     )
 }
 
