41f938a6edfafc9c2ad419fce5c7b22e1308e9a3
Cayden <clearlyrubix@gmail.com>
2024-04-25 20:35:36 -0600

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,23 @@
 
 import AboutSection from "./components/sections/about/AboutSection";
 import HeroSection from "./components/sections/HeroSection";
+import FeaturedProjectsSection from "./components/sections/projects/FeaturedProjectsSection";
 
 export default function Home() {
   
     return (
         <main className="w-full">
-            <div className="flex flex-col md:flex-row w-full">
-                <div className="basis-1/2 self-stretch">
+            <div className="flex flex-col md:flex-row w-full items-stretch">
+                <div className="basis-1/2">
                     <HeroSection />
                 </div>
-                <div className="basis-1/2 self-stretch">
+                <div className="basis-1/2">
                     <AboutSection/>
                 </div>
+            </div>
+            <div>
+                <FeaturedProjectsSection/>
+                Test
             </div>
         </main>
     );
