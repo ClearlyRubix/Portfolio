0527bbd014c5b861486f5ccc8420f41a71ec4088
Cayden <clearlyrubix@gmail.com>
2024-04-25 20:38:41 -0600

    styling fix

--- src/app/components/sections/contact/ContactSection.tsx
+++ src/app/components/sections/contact/ContactSection.tsx
@@ -6,12 +6,12 @@ import ContactText from "./ContactText"
 const ContactSection = () => {
     return (
         <>
-        <div id="contact" className="flex flex-col border-2 border-zinc-800 rounded m-4 p-4 h-full">
+        <div id="contact" className="flex flex-col border-2 border-zinc-800 rounded m-4 p-4">
             <h2 className="font-bold text-sm text-zinc-100 uppercase tracking-widest pb-6">
                 Let&#39;s Connect
             </h2>
             <ContactText/> 
-            <ul className="flex items-center md:mt-auto">
+            <ul className="flex items-center mt-8">
                 <li className="mr-5 shrink-0 transition-transform hover:-translate-y-1">
                     <Link className="hover:text-zinc-200" href={"https://www.linkedin.com/in/caydenbatten/"} target="_blank" title="LinkedIn">
                         <IconLinkedIn feather={true}/>
