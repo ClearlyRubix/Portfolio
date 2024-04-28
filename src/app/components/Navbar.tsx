72bcaccf34e22cbe994b450f432677b9dbc7d7d1
Cayden <clearlyrubix@gmail.com>
2024-04-27 18:23:50 -0600

    Centered Name on desktop

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,20 +9,21 @@ const Navbar = () => {
     return (
         <>
         <nav className="border-b-zinc-800 border-b-2 border-x-transparent border-t-transparent bg-zinc-900">
-            <div className="bg-zinc-900 flex flex-wrap justify-between mx-auto items-center pt-2 pb-1 md:pb-2">
+            <div className="bg-zinc-900 flex flex-wrap justify-between mx-auto items-center pt-2 pb-1 md:pb-2 ">
+                <div className="hidden md:block md:w-1/3"></div>
                 <div>
                     <Link href={"/"} className="text-2xl md:text-3xl text-zinc-200 font-bold uppercase tracking-widest inline-flex py-1 px-3">
                         Cayden
                     </Link>
                 </div>
-                <div className="w-auto">
+                <div className="md:w-1/3 flex flex-row justify-end">
                     <button onClick={() => setnavDropDownEnabled(!navDropDownEnabled)} type="button" className="inline-flex items-center md:hidden text-zinc-400 py-1 px-4">
                         <span className="sr-only">Open Menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                         </svg>
                     </button>
-                    <div>
+                    <div className="inline">
                         <div className="md:block hidden">
                             <ul className="inline-flex justify-between">
                                     <li key="about" className="transition-transform hover:-translate-y-1"><Link href={"/#about"} className="text-zinc-400 sm:text-xl rounded font-semibold hover:text-white mx-3 my-1">About</Link></li>
