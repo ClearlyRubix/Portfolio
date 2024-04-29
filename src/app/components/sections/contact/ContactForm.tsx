4d52d375eae93c5f9054b4ce5af0c98ffcda3406
Cayden <clearlyrubix@gmail.com>
2024-04-28 19:22:07 -0600

    contact field checking and disable the button on submit to ensure no double sends

--- src/app/components/sections/contact/ContactForm.tsx
+++ src/app/components/sections/contact/ContactForm.tsx
@@ -1,108 +1,137 @@
-"use client"
-import { FormEvent, useState } from "react"
-
+"use client";
+import { FormEvent, useState } from "react";
+import IconLoader from "../../icons/IconLoader";
 
 const ContactForm = () => {
-    const [emailSubmitted, setEmailSubmitted] = useState(false)
-    const [emailError, setEmailError] = useState(false)
+    const [emailSubmitted, setEmailSubmitted] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [sendButtonDisabled, setSendButtonDisabled] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("There was an error.")
 
     const handleSubmit = async (formData: FormData) => {
+        setSendButtonDisabled(true);
 
-        const rawFormData = {
-            email: formData.get("email"),
-            subject: formData.get("subject"),
-            message: formData.get("message")
+        const email = formData.get("email")
+        const subject = formData.get("subject")
+        const message = formData.get("message")
+
+        if (email == null || subject == null || message == null) {
+            setSendButtonDisabled(false)
+            setEmailError(true);
+            setEmailSubmitted(false);
+            setErrorMessage("There was a problem sending the emai. Make sure all fields are filled in.")
+            return
         }
-    
-        const JSONData = JSON.stringify(rawFormData)
-        const endpoint = "api/sendEmail"
-    
+
+        if (email?.toString().length == 0 || subject?.toString().length == 0 || message?.toString().length == 0) {
+            setSendButtonDisabled(false)
+            setEmailError(true);
+            setEmailSubmitted(false);
+            setErrorMessage("There was a problem sending the emai. Make sure all fields are filled in.")
+            return
+        }
+        const rawFormData = {
+            email: email,
+            subject: subject,
+            message: message,
+        };
+
+        const JSONData = JSON.stringify(rawFormData);
+        const endpoint = "api/sendEmail";
+
         const options = {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSONData
-        }
-        
-        setEmailError(false)
-        const response = await fetch(endpoint, options)
-        // const resData = await response.json()
-        console.log(response)
+            body: JSONData,
+        };
+
+        setEmailError(false);
+        const response = await fetch(endpoint, options);
+        // const response = { // testing purposes
+        //     status: 200
+        // }
+
         if (response.status === 200) {
-            setEmailSubmitted(true)
+            setEmailSubmitted(true);
         } else {
-            setEmailError(true)
-            setEmailSubmitted(false)
+            setEmailError(true);
+            setEmailSubmitted(false);
+            setSendButtonDisabled(false);
+            setErrorMessage("There was a problem sending the emai. Please try again later.")
         }
-    
-    }
+    };
 
     return (
-        <div id="contact" className="flex flex-col border-2 border-zinc-800 rounded m-4 p-4">
+        <div
+            id="contact"
+            className="flex flex-col border-2 border-zinc-800 rounded m-4 p-4"
+        >
             <h2 className="font-bold text-sm text-zinc-100 uppercase tracking-widest pb-6">
                 Contact Me
             </h2>
-            {emailSubmitted ? 
+            {emailSubmitted ? (
                 <div>
                     <p className="mt-2">Email Submitted Successfully!</p>
                 </div>
-            :
-            <>
-            {emailError ? 
-            <p className="mb-2 text-red-500">There was a problem sending the emai. Please try again later.</p>    
-            :
-            <></>
-            }
-            <form 
-                action={handleSubmit}
-                className="flex flex-col text-sm w-full"
-            >
-            <div className="mb-6">
-                <label className="block mb-2">
-                    Your email
-                </label>
-                <input
-                    placeholder="name@example.com"
-                    type="email"
-                    name="email"
-                    className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
-
-                />
-            </div>
-            <div className="mb-6">
-                <label className="block mb-2">
-                    Subject
-                </label>
-                <input
-                    placeholder="Hi Cayden, I'd like to hire you!"
-                    type="text"
-                    name="subject"
-                    className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
-                />
-            </div>
-            <div className="mb-6">
-                <label className="block mb-2">
-                    Message
-                </label>
-                <textarea
-                    placeholder="Let's talk about..."
-                    name="message"
-                    className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
-                />
-            </div>
-            <button
-                type="submit"
-                className="text-md font-medium py-2 px-3 border-2 border-zinc-200 rounded-lg w-fit hover:bg-zinc-800"
-            >
-                Send Message
-            </button>
-        </form>
-        </>
-        }
-            
+            ) : (
+                <>
+                    {emailError ? (
+                        <p className="mb-2 text-red-500">
+                            {errorMessage}
+                        </p>
+                    ) : (
+                        <></>
+                    )}
+                    <form
+                        action={handleSubmit}
+                        className="flex flex-col text-sm w-full"
+                    >
+                        <div className="mb-6">
+                            <label className="block mb-2">Your email</label>
+                            <input
+                                placeholder="name@example.com"
+                                type="email"
+                                name="email"
+                                className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block mb-2">Subject</label>
+                            <input
+                                placeholder="Hi Cayden, I'd like to hire you!"
+                                type="text"
+                                name="subject"
+                                className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block mb-2">Message</label>
+                            <textarea
+                                placeholder="Let's talk about..."
+                                name="message"
+                                className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
+                            />
+                        </div>
+                        {sendButtonDisabled ? (
+                            <div>
+                                <IconLoader />
+                            </div>
+                        ) : (
+                            <button
+                                type="submit"
+                                disabled={false}
+                                className="text-md font-medium py-2 px-3 border-2 border-zinc-200 rounded-lg w-fit hover:bg-zinc-800"
+                            >
+                                Send Message
+                            </button>
+                        )}
+                    </form>
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default ContactForm
+export default ContactForm;
