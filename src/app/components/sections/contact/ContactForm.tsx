24c3b46bc3a5f2f02e1316c1b9244972e0fd58ed
Cayden <18541769+ClearlyRubix@users.noreply.github.com>
2024-05-10 12:41:55 -0600

    form action was causing the loading wheel and button to not update properly. Fixed with onSubmit handler instead

--- src/app/components/sections/contact/ContactForm.tsx
+++ src/app/components/sections/contact/ContactForm.tsx
@@ -1,33 +1,42 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import IconLoader from "../../icons/IconLoader";
 
 const ContactForm = () => {
     const [emailSubmitted, setEmailSubmitted] = useState(false);
     const [emailError, setEmailError] = useState(false);
     const [sendButtonDisabled, setSendButtonDisabled] = useState(false);
+    const isSubmitting = useRef(false)
     const [errorMessage, setErrorMessage] = useState("There was an error.")
 
-    const handleSubmit = async (formData: FormData) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (isSubmitting.current) return // ensure no duplicate submissions occur
+
+        isSubmitting.current = true
         setSendButtonDisabled(true);
+
+        const formData = new FormData(event.currentTarget)
 
         const email = formData.get("email")
         const subject = formData.get("subject")
         const message = formData.get("message")
 
         if (email == null || subject == null || message == null) {
-            setSendButtonDisabled(false)
+            
+            //setSendButtonDisabled(false)
             setEmailError(true);
             setEmailSubmitted(false);
-            setErrorMessage("There was a problem sending the emai. Make sure all fields are filled in.")
+            setErrorMessage("There was a problem sending the email. Make sure all fields are filled in.")
             return
         }
 
         if (email?.toString().length == 0 || subject?.toString().length == 0 || message?.toString().length == 0) {
-            setSendButtonDisabled(false)
+            
+            //setSendButtonDisabled(false)
             setEmailError(true);
             setEmailSubmitted(false);
-            setErrorMessage("There was a problem sending the emai. Make sure all fields are filled in.")
+            setErrorMessage("There was a problem sending the email. Make sure all fields are filled in.")
             return
         }
         const rawFormData = {
@@ -59,6 +68,7 @@ const ContactForm = () => {
             setEmailError(true);
             setEmailSubmitted(false);
             setSendButtonDisabled(false);
+            isSubmitting.current = false
             setErrorMessage("There was a problem sending the emai. Please try again later.")
         }
     };
@@ -85,7 +95,7 @@ const ContactForm = () => {
                         <></>
                     )}
                     <form
-                        action={handleSubmit}
+                        onSubmit={handleSubmit}
                         className="flex flex-col text-sm w-full"
                     >
                         <div className="mb-6">
@@ -114,6 +124,9 @@ const ContactForm = () => {
                                 className="block w-full bg-zinc-800 border-2 border-zinc-200 rounded-md p-2"
                             />
                         </div>
+                        <div>
+                            sendButtonDisabled: {sendButtonDisabled.toString()}
+                        </div>
                         {sendButtonDisabled ? (
                             <div>
                                 <IconLoader />
@@ -121,7 +134,7 @@ const ContactForm = () => {
                         ) : (
                             <button
                                 type="submit"
-                                disabled={false}
+                                disabled={sendButtonDisabled}
                                 className="text-md font-medium py-2 px-3 border-2 border-zinc-200 rounded-lg w-fit hover:bg-zinc-800"
                             >
                                 Send Message
